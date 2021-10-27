f418fcc1b06f26cf7c2c8cb2e610cefa309327e0
Elber Martinez <elber.martinez.mt@correo.usa.edu.co>
2021-10-27 06:57:59 -0500

    Primer commit del proyecto gha

---
+++ src/main/java/com/mycompany/gha/Clase1.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.gha;
+
+/**
+ *
+ * @author elber
+ */
+public class Clase1 {
+    public int sumar (int a, int b){
+        return a + b;
+    }
+}
---
+++ src/test/java/com/mycompany/gha/Clase1Test.java
@@ -0,0 +1,50 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit5TestClass.java to edit this template
+ */
+package com.mycompany.gha;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ *
+ * @author juvinao
+ */
+public class Clase1Test {
+    
+    public Clase1Test() {
+    }
+    
+    @BeforeAll
+    public static void setUpClass() {
+    }
+    
+    @AfterAll
+    public static void tearDownClass() {
+    }
+    
+    @BeforeEach
+    public void setUp() {
+    }
+    
+    @AfterEach
+    public void tearDown() {
+    }
+
+    /**
+     * Test of sumar method, of class Clase1.
+     */
+    @Test
+    public void testSumar() {
+        System.out.println("sumar");
+        Clase1 instance = new Clase1();
+        int result = instance.sumar(5, 4);
+        assertEquals(9, result);
+    }
+    
+}
